ccb4ee5ece39c0a5fa370fecf1e986ef0a2c878c
Kevin <jpen0010@student.monash.edu>
2024-08-05 03:46:47 +1000

    update

--- static/accidents_data.js
+++ static/accidents_data.js
@@ -3,10 +3,10 @@ import json
 
 def fetch_accident_data():
     conn = psycopg2.connect(
-        dbname="postgres",
+        dbname="victoria",
         user="postgres",
         password="a7316713",
-        host="database-2.c9e42ai486qt.ap-southeast-2.rds.amazonaws.com"
+        host="localhost"
     )
     cur = conn.cursor()
 
